8064cca5b217f578df1382be1e5f5d5c68909782
Johnny Gérard <john@jgerard.dev>
2026-01-11 13:07:52 +0100

    refactor: move source code inside src directory

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { schemaTypes } from "./schema-types";
+import { schemaTypes } from "./src/schema-types";
 
 export default defineConfig({
   name: "default",
